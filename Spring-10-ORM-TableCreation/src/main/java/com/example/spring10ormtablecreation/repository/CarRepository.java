6945d40ac11624987649e6700f88d9d74deb15c8
Jose <josephum.garcia@gmail.com>
2022-04-19 19:32:15 -0400

    Spring Data

--- Spring-10-ORM-TableCreation/src/main/java/com/example/spring10ormtablecreation/repository/CarRepository.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/example/spring10ormtablecreation/repository/CarRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CarRepository extends JpaRepository<Car,Long> {
 
-    //custom methods
-    //sql
+    //custom methods - sql queries
+
 }
---
+++ Spring-11-ORM-Mapping-OneToOne/src/main/java/com/example/entity/Department.java
@@ -0,0 +1,24 @@
+package com.example.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "departments")
+@NoArgsConstructor
+@Data
+public class Department extends BaseEntity{
+
+    private String department;
+    private String division;
+
+    public Department(String department, String division) {
+        this.department = department;
+        this.division = division;
+    }
+}
